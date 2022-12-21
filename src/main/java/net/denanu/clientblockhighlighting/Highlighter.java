b1eb3dbce4e4b96b31c3449e705366f444c9f068
Julian Wandhoven <Julian.wandhoven@gmail.com>
2022-12-21 22:22:27 +0100

    Added simple API

---
+++ src/main/java/net/denanu/clientblockhighlighting/Highlighter.java
@@ -0,0 +1,40 @@
+package net.denanu.clientblockhighlighting;
+
+import java.util.Collection;
+
+import net.denanu.clientblockhighlighting.components.ChunkComponents;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+
+public class Highlighter {
+	public static void highlight(final Chunk chunk, final Identifier key, final BlockPos pos) {
+		ChunkComponents.HIGHLIGHTS.get(chunk).add(key, pos);
+	}
+
+	public static void highlight(final Chunk chunk, final Identifier key, final Collection<BlockPos> pos) {
+		ChunkComponents.HIGHLIGHTS.get(chunk).addAll(key, pos);
+	}
+
+	public static void unhighlight(final Chunk chunk, final Identifier key, final BlockPos pos) {
+		ChunkComponents.HIGHLIGHTS.get(chunk).remove(key, pos);
+	}
+
+	public static void unhighlight(final Chunk chunk, final Identifier key, final Collection<BlockPos> pos) {
+		ChunkComponents.HIGHLIGHTS.get(chunk).removeAll(key, pos);
+	}
+
+	public static void highlight(final ServerWorld world, final Identifier key, final BlockPos pos) {
+		ChunkComponents.HIGHLIGHTS.get(Highlighter.getChunk(world, pos)).add(key, pos);
+	}
+
+
+	public static void unhighlight(final ServerWorld world, final Identifier key, final BlockPos pos) {
+		ChunkComponents.HIGHLIGHTS.get(Highlighter.getChunk(world, pos)).remove(key, pos);
+	}
+
+	private static Chunk getChunk(final ServerWorld world, final BlockPos pos) {
+		return world.getChunk(pos);
+	}
+}
